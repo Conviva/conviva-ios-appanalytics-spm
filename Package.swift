96668fbeae26717cea8b9b500db440387a4eb4ee
Butchi Peddi <bpeddi@conviva.com>
2022-04-21 14:26:24 +0530

    SPM Support

--- Package.swift
+++ Package.swift
@@ -5,7 +5,7 @@ import PackageDescription
 let package = Package(
     name: "ConvivaAppAnalytics",
     platforms: [
-        .iOS(.v12)
+        .iOS(.v12),
         .tvOS(.v12)
     ],
     products: [
