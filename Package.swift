2dfbaa0803a71333034c07a64d6ef8972b09f52f
Butchi Peddi <58072398+butchi-conviva@users.noreply.github.com>
2022-04-25 11:56:51 +0530

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -8,24 +8,24 @@ let package = Package(
         .iOS(.v12),
         .tvOS(.v12)
     ],
-    dependencies: [
-        .package(
-            name: "FMDB", 
-            url: "https://github.com/ccgus/fmdb", 
-            .upToNextMinor(from: "2.7")),
-    ],
+ 
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "ConvivaAppAnalytics",
             targets: ["ConvivaAppAnalytics"])
     ],
+   dependencies: [
+        .package(
+            name: "FMDB", 
+            url: "https://github.com/ccgus/fmdb", 
+            .upToNextMinor(from: "2.7.8")),
+    ],
     targets: [
         .binaryTarget(
             name: "ConvivaAppAnalytics",
             url: "https://github.com/Conviva/conviva-ios-appanalytics/raw/0.2.1/Framework/ConvivaAppAnalytics.xcframework.zip",
-            checksum: "c7abd09bb4a3b7f7066363c67066ace431bbd0b2cdd2769b8edd9b6482ae1345",
-            dependencies:["FMDB"])
+            checksum: "c7abd09bb4a3b7f7066363c67066ace431bbd0b2cdd2769b8edd9b6482ae1345")
         
      ]
 )
